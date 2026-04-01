80e4af2ef60d2bb87450f7b7f6d1e34603291bf7
MMaverick149 <mmaverick1400@gmail.com>
2026-04-01 12:09:52 +0200

    add: login,style,script

---
+++ script.js
@@ -0,0 +1,7 @@
+function login() {
+    // Tady můžeš přidat zvukový efekt nebo animaci před vstupem
+    console.log("Přístup povolen...");
+    
+    // Přesměrování na hlavní panel MDT
+    window.location.href = "index.html"; 
+}
