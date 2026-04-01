2ab6c34517a77bb4549c942a00e5abc41ef1085f
MMaverick149 <mmaverick1400@gmail.com>
2026-04-01 12:11:59 +0200

    fix mby

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
 function login() {
-    // Tady můžeš přidat zvukový efekt nebo animaci před vstupem
-    console.log("Přístup povolen...");
-    
-    // Přesměrování na hlavní panel MDT
     window.location.href = "index.html"; 
 }
